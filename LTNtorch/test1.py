4befc49ed2b0307d80dd1905b420c68ebf45c557
Zhijin Yang <zyang44@ncsu.edu>
2024-03-31 21:52:59 -0400

    set iris dataset some zero for test

--- LTNtorch/test1.py
+++ LTNtorch/test1.py
@@ -1,4 +1,21 @@
-import os
+import pandas as pd
 
-# 打印当前工作目录
-print("当前工作目录:", os.getcwd())
+# 定义文件路径
+training_file = 'iris_training_withzero.csv'
+test_file = 'iris_test_withzero.csv'
+
+# 处理训练数据集
+train_data = pd.read_csv(training_file)
+# 将第三列的值设置为0
+train_data.iloc[:, 2] = 0
+# 保存修改后的DataFrame回文件
+train_data.to_csv(training_file, index=False)
+
+# 处理测试数据集
+test_data = pd.read_csv(test_file)
+# 将第三列的值设置为0
+test_data.iloc[:, 2] = 0
+# 保存修改后的DataFrame回文件
+test_data.to_csv(test_file, index=False)
+
+print("第三列值已成功修改为0。")
