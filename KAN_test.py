6537daa985dc0dfc1180ecdfb38f0251433f1311
zhijin44 <zyang44@ncsu.edu>
2025-02-25 16:34:05 -0500

    delete some unuseful files

--- KAN_test.py
+++
@@ -1,8 +0,0 @@
-from kan import *
-torch.set_default_dtype(torch.float64)
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(device)
-
-# create a KAN: 2D inputs, 1D output, and 5 hidden neurons. cubic spline (k=3), 5 grid intervals (grid=5).
-model = KAN(width=[2,5,1], grid=3, k=3, seed=42, device=device)
